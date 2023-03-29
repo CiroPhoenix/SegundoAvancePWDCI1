8a96bff3273a889f0a205506bd92b5d7805c2189
CiroPhoenix <111394426+CiroPhoenix@users.noreply.github.com>
2023-03-28 22:45:35 -0600

    Add files via upload

--- PWDCISegundoAvance/editar.php
+++ PWDCISegundoAvance/editar.php
@@ -80,9 +80,9 @@ if(isset($_POST["Editar"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar Usuario</title>
+    <title>Editar Usuario</title>
     <link rel="stylesheet" href="css/estilos.css" />
-    <link rel="stylesheet" href="css/estilo-registrer-login.css" />
+    <link rel="stylesheet" href="css/editor.css" />
     
 
     <style>
